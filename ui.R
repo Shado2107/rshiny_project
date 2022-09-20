dd0c464e46504efdd77550cf22d66578b20da08b
ousseini <adnanejiko@gmail.com>
2022-09-20 17:31:01 +0000

    affichage des données

--- ui.R
+++ ui.R
@@ -1,32 +1,6 @@
-
-library(shiny)
-library(shinydashboard)
-
-
-ui = dashboardPage( skin= "red",
-                    dashboardHeader(title ="Super cool header"),
-                    dashboardSidebar(
-                      sidebarMenu(
-                        #upload file option
-                        fileInput("myFile","Upload Excel Data:", accept = ".xlsx"),
-                        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-                        menuItem("widgets", tabName = "widgets", icon = icon("th"))
-                      )
-                    ),
-                    dashboardBody(
-                      fluidRow(
-                        # A static valueBox
-                        valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
-                        
-                        # A static valueBox
-                        valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
-                        
-                        # A static valueBox
-                        valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
-                       
-                      )
-                      
-                    )
-  
-)
-
+ui <- fluidPage(titlePanel("PRODUITS AUCHAN LES PLUS VENDUS"),
+                sidebarLayout(sidebarPanel(
+                  fileInput('file1', 'Choisir un fichier excel',
+                            accept = c(".xlsx"))
+                ),
+                mainPanel(tableOutput('file2'))))
