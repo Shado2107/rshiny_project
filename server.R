e45d73298b747575c055a7e512565361a8f4f1b2
Shado2107 <pd39almeida@outlook.com>
2022-09-25 11:27:30 +0000

    update

--- server.R
+++ server.R
@@ -1,28 +1,143 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinyWidgets)
+library(shinydashboard)
+library(ggplot2)
+library(plotly)
+library(readxl)
+library(FactoMineR)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
 
-    })
+#dataset = read.csv("credit.xlsx",header = TRUE, dec = ".", col.names = c("Marche", "Impaye","Endettement","Famille","Profession","Age"), row.names = NULL)
+
+
+shinyServer(function(input, output, session){
+  
+  #uploading the excel file
+  #dataset = reactive({
+   # file <- input$myFile
+    #req(file)
+    #data <- read_excel(file$datapath)
+    #data <- as.data.frame(data)
+    # return(data)
+  #})
+  
+  dataset <- reactiveValues(mydata=NULL)
+  
+  observeEvent(input$myFile,{
+    ext <- tools::file_ext(input$myFile$name)
+    
+    if(ext == "xls" || ext == "xlsw"){
+      dataset$mydata <- as.data.frame(readxl::read_excel(input$myFile$datapath)) 
+    } else {
+      tryCatch({
+        dataset$mydata <- read.csv(input$myFile$datapath,
+                                   header = input$header,
+                                   sep = input$sep,
+                                   quote = input$quote)
+      }, 
+      error = function(e){
+        stop(safeError(e))
+      })
+    }
+    dataset$mydata <- na.omit(dataset$mydata)
+    dataset$mydata <- dataset$mydata[complete.cases(dataset$mydata),]
+  })
+  
+ 
+
+  
+  
+  output$contents <- DT::renderDataTable({
+    DT::datatable(dataset$mydata,
+                  rownames = FALSE,
+                  width = NULL,
+                  height = 50,
+                  editable = TRUE,
+                  selection = list(mode = "single", selected = c(1), target = 'row'),
+                  fillContainer = getOption("DT.fillContainer", TRUE),
+                  options = list(
+                    lengthMenu = list(c(10, 25, 50,-1), c('10', '25','50' ,'All')),
+                    paging = TRUE,
+                    lenthChange=TRUE,
+                    searching = FALSE,
+                    fixedColumns = FALSE,
+                    autoWidth = FALSE,
+                    ordering = FALSE
+                  ),
+                  class ='cell-border stripe compact white-space: nowrap',
+                  )
+      
+  })
+  
+  output$Summary <- renderDataTable({
+    summary(dataset$mydata)
+  })
+  
+  
+  
+  output$donut1 <- renderPlotly({
+    newdata2 <- dataset$mydata %>% 
+      group_by(Profession) %>%
+      summarise(count= n())
+    
+    return(plot_ly(newdata2, labels = ~Profession, values = ~ count, type = "pie", hole = 0.6,
+                   marker = list(colors = c('#635f5f','#3486f9'),line = list(color = '#635f5f', width = 2)))%>%
+             
+             layout(title = "Profession",  showlegend = T,
+                    xaxis = list(showgrid = FALSE, zeroline = FALSE, 
+                                 showticklabels = FALSE),
+                    yaxis = list(showgrid = FALSE, zeroline = FALSE, 
+                                 showticklabels = FALSE)) %>%
+             config(displayModeBar = F) %>% layout(xaxis=list(fixedrange=TRUE)) %>% 
+             layout(yaxis=list(fixedrange=TRUE)))
+  
+  })
+  
+  
+  output$donut2 <- renderPlotly({
+    newdata3 <- dataset$mydata %>% 
+      group_by(Marche) %>%
+      summarise(count= n())
+    
+    return(plot_ly(newdata3, labels = ~Marche, values = ~ count, type = "pie", hole = 0.6,
+                   marker = list(colors = c('#635f5f','#3486f9'),line = list(color = '#635f5f', width = 2)))%>%
+             layout(title = "Marche",  showlegend = T,
+                    xaxis = list(showgrid = FALSE, zeroline = FALSE, 
+                                 showticklabels = FALSE),
+                    yaxis = list(showgrid = FALSE, zeroline = FALSE, 
+                                 showticklabels = FALSE)) %>%
+             config(displayModeBar = F) %>% layout(xaxis=list(fixedrange=TRUE)) %>% 
+             layout(yaxis=list(fixedrange=TRUE)))
+    
+  })
+  
+
+  output$plot1 <- renderPlot({
+   
+    
+  })
+  
+  
+  
+  
+  output$hist1 <- renderPlot({
+    
+    tab.credit=table(dataset$mydata$Impaye,dataset$mydata$Famille)
+    tab.credit=as.data.frame(as.matrix(tab.credit))
+    credit.afc=CA(tab.credit,graph = FALSE)
+    val.prop <- credit.afc$eig
+    
+    
+    barplot(val.prop[,2], names.arg = 1:nrow(val.prop),
+            main = "%Variances expliquées par les axes principaux",
+            xlab = "Composantes principales",
+            ylab = "Pourcentage de variances",
+            col ="steelblue")
+    lines(x = 1:nrow(val.prop),y = val.prop[,2],type = "b", pch = 15 ,col = "yellow")
+    
+  })
 
 })
--- ui.R
+++ ui.R
@@ -1,32 +1,70 @@
 
 library(shiny)
 library(shinydashboard)
+library(plotly)
 
 
-ui = dashboardPage( skin= "red",
-                    dashboardHeader(title ="Super cool header"),
-                    dashboardSidebar(
-                      sidebarMenu(
-                        #upload file option
-                        fileInput("myFile","Upload Excel Data:", accept = ".xlsx"),
-                        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-                        menuItem("widgets", tabName = "widgets", icon = icon("th"))
-                      )
-                    ),
-                    dashboardBody(
-                      fluidRow(
-                        # A static valueBox
-                        valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
-                        
-                        # A static valueBox
-                        valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
-                        
-                        # A static valueBox
-                        valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
-                       
-                      )
-                      
-                    )
+
+marche_options <- c("No Selection", "Renovation", "Voiture","Scooter","Mobilier / Ameublement")
+famille_options <- c("No Selection", "Union libre", "Marie","Veuf","Divorce","Celibataire")
+professions_options <- c("No Selection", "Ouvrier non qualifie", "Ouvrier qualifie","Retraite","Cadre moyen","Cadre sup")
+
+
+ui <- dashboardPage(skin = "purple", 
+                dashboardHeader(title = "Credit analysis", titleWidth = 250),
+                dashboardSidebar( width = 300,
+                                  sidebarMenu(style = "position: fixed; width:250px;",
+                                              fileInput("myFile",
+                                                        "Upload Excel Data:",
+                                                        multiple = FALSE,
+                                                        accept = c(".xls", ".xslx",".csv")),
+                                              
+                                              checkboxInput("header", "Header", TRUE),
+                                              
+                                              radioButtons("sep", "Separator",
+                                                           choices = c(Comma = ",",
+                                                                       Semicolon = ";",
+                                                                       Tab = "\t"),
+                                                           selected = ","),
+                                              
+                                              radioButtons("quote", "Quote",
+                                                           choices = c(None = "",
+                                                                       "Double Qot." = '"',
+                                                                       "Single Qot." = "'"),
+                                                           selected = '"'),
+                                              
+                                              radioButtons("endettement", "Select type endetement:", choices = 
+                                                             c("End_1", "End_2", "End_3","End_4")),
+          
+                                             
+                                              selectInput("marche", 
+                                                          label = "Marche :",
+                                                          choices = c(marche_options), 
+                                                          selected = "No Selection")
+                                  )
+                                  
+                ),
+                
+  dashboardBody(
+    fluidRow(
+      
+      #histogram is being added here
+      box(width = 6, plotlyOutput("hist1")),
+    
+      box(width = 6, plotOutput("plot1")),
+      
+      #donut chart is being added here
+      box(width = 6, plotlyOutput("donut1")),
+      
+      #donut chart is being added here
+      box(width = 6, plotlyOutput("donut2")),
+      
+      box(width = 12, dataTableOutput("Summary")),
+      
+      box(width = 12, DT::dataTableOutput("contents"))
+      
+    ),
+   
+  )
+  )
   
-)
-
